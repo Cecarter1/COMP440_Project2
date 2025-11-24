37deb80de32d780217802532e05114fee04629bd
rstubbs <rstubbs@aggies.ncat.edu>
2025-11-24 18:38:39 -0500

    Prototype3
    -Final changes for prototype3

---
+++ Assets/Scripts/RashadScripts/SceneChanger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
